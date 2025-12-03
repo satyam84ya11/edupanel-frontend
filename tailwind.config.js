40fd305c8ca3b710144a42780aee9b926d22bc95
satyam <satyam84ya06@gmail.com>
2025-12-03 19:42:01 +0530

    fixed tailwind build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#2563eb',
-        secondary: '#1e3a8a',
-        accent: '#93c5fd',
-      },
-      fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-      },
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
     },
-  },
-  plugins: [],
-};
-
-
+    plugins: [],
+  };
+  
